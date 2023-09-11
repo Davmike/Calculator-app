d0d85e9a08178d78b82b13c8cf98b37a93885dcc
David <davidmikeladze00@gmail.com>
2023-09-11 22:32:19 +0400

    feat: add js page

---
+++ script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const rangeInput = document.getElementById("rangeInput");
+const numberResult = document.querySelector(".number-result");
+const number = document.querySelector(".number");
