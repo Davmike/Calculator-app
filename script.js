f996f382c9f9858dc3d72ab9edc02b593a4c9852
David <davidmikeladze00@gmail.com>
2023-09-12 14:15:35 +0400

    feat: i make a new calculator style in css

--- script.js
+++ script.js
@@ -3,7 +3,13 @@
 const rangeInput = document.getElementById("rangeInput");
 const numberResult = document.querySelector(".number-result");
 const number = document.querySelector(".number");
+const body = document.body;
 
 rangeInput.addEventListener("input", (event) => {
-  console.log(event.target.value);
+  if (event.target.value === "2") {
+    body.classList.remove("theme-3");
+    body.classList.add("theme-2");
+  } else {
+    body.classList.add("theme-3");
+  }
 });
