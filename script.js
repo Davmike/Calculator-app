abcadeaf554b93143d37d82ee91c7f748ff51185
David <davidmikeladze00@gmail.com>
2023-09-12 13:01:07 +0400

    fix: change web kit to apperance

--- script.js
+++ script.js
@@ -3,3 +3,7 @@
 const rangeInput = document.getElementById("rangeInput");
 const numberResult = document.querySelector(".number-result");
 const number = document.querySelector(".number");
+
+rangeInput.addEventListener("input", (event) => {
+  console.log(event.target.value);
+});
