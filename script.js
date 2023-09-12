f2931b20b66a32850eca128f33885b012fa4f657
David <davidmikeladze00@gmail.com>
2023-09-12 17:20:51 +0400

    feat: add when i click button text change with button number

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 
 const rangeInput = document.getElementById("rangeInput");
 const numberResult = document.querySelector(".number-result");
-const number = document.querySelector(".number");
+const number = document.querySelectorAll(".number");
 const body = document.body;
+
 
 rangeInput.addEventListener("input", (event) => {
   if (event.target.value === "1") {
@@ -14,9 +15,15 @@ rangeInput.addEventListener("input", (event) => {
     body.classList.remove("theme-3");
     body.classList.add("theme-2");
   }
-
   if (event.target.value === "3") {
     body.classList.remove("theme-2");
     body.classList.add("theme-3");
   }
+});
+
+number.forEach((element) => {
+  element.addEventListener("click", () => {
+    const buttonText = element.textContent;
+    numberResult.textContent = buttonText;
+  });
 });
