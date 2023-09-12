7cb13b0f6aa4b88243d18f280f271a64a9a7cf1b
David <davidmikeladze00@gmail.com>
2023-09-12 15:29:03 +0400

    feat: i make a js input theme changer

--- script.js
+++ script.js
@@ -6,10 +6,17 @@ const number = document.querySelector(".number");
 const body = document.body;
 
 rangeInput.addEventListener("input", (event) => {
+  if (event.target.value === "1") {
+    body.classList.remove("theme-2");
+    body.classList.remove("theme-3");
+  }
   if (event.target.value === "2") {
     body.classList.remove("theme-3");
     body.classList.add("theme-2");
-  } else {
+  }
+
+  if (event.target.value === "3") {
+    body.classList.remove("theme-2");
     body.classList.add("theme-3");
   }
 });
